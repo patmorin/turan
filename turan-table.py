195f46c2700524e9787a923c90218ac14b691aa2
Pat Morin <morin@scs.carleton.ca>
2017-01-31 12:09:27 -0500

    Soooooo cloooose

--- turan-table.py
+++ turan-table.py
@@ -75,7 +75,7 @@ def make_table(fp, upper_bounds, lower_bounds):
     rowcfgs = [taco, bat, nested, crossing]
     cols = sorted([pack_set(s) for s in powerset(colcfgs)])
     rows = sorted([pack_set(s) for s in powerset(rowcfgs)])
-
+    tight = 0
     fp.write(r'\begin{{tabular}}{{|c@{{\,}}c@{{\,}}c@{{\,}}c{}|}}\hline'.format('|c'*8) + '\n')
     for i in colcfgs:
         fp.write("&&&&")
@@ -92,11 +92,13 @@ def make_table(fp, upper_bounds, lower_bounds):
             x = r|c
             if upper_bounds[x] == lower_bounds[x]:
                 fp.write(r'&${}$'.format(format_exponent(upper_bounds[x])))
+                tight += 1
             else:
                 fp.write(r'&${}/{}$'.format(format_exponent(lower_bounds[x]),
                                             format_exponent(upper_bounds[x])))
         fp.write(r'\\ \hline' + '\n')
     fp.write(r'\end{tabular}')
+    print("Found {} tight bounds".format(tight))
 
 if __name__ == "__main__":
 
@@ -119,7 +121,7 @@ if __name__ == "__main__":
     upper_bounds[pack(nested, crossing)] = 2
     upper_bounds[pack(bat, crossing)] = 2
 
-    # These are Brass, Rote, and Swanepoel
+    # This is Brass, Rote, and Swanepoel
     upper_bounds[pack(ears, swords, bat, nested)] = 1
 
     # These are inherited from hypergraphs
@@ -144,6 +146,8 @@ if __name__ == "__main__":
     # More linear upper bounds
     upper_bounds[pack(taco, nested, david)] = 1
     upper_bounds[pack(taco, nested, crossing)] = 1
+    upper_bounds[pack(nested, ears, crossing)] = 1
+    upper_bounds[pack(nested, bat, david)] = 1 # TODO: prove this
 
     # Tripod packing
     lower_bounds[pack(taco, nested, bat, ears)] = 1.546  # Gowers and Long
@@ -154,16 +158,13 @@ if __name__ == "__main__":
     lower_bounds[pack(taco, david, crossing, bat, ears)] = 2
     lower_bounds[pack(swords, bat, ears, david)] = 2
     lower_bounds[pack(nested, ears, david)] = 2
+    lower_bounds[pack(david, nested, crossing)] = 2
+    lower_bounds[pack(bat, nested, ears)] = 2
 
     # The butterfly is irrelevant
     s = set(range(8))-{mariposa}
     for t in s:
         lower_bounds[pack_set(s-{t})] = 1
-
-
-    #print_everything(upper_bounds, lower_bounds)
-    print("swords={}".format(upper_bounds[pack(swords)]))
-    print("taco={}".format(upper_bounds[pack(taco)]))
 
     # Mariposas don't matter
     for k in list(upper_bounds.keys()):
@@ -174,11 +175,6 @@ if __name__ == "__main__":
             print("Warning: upper bound {} != {}".format(tostring(x), tostring(k)))
         upper_bounds[x] = upper_bounds[k]
 
-    if pack(ears,taco) in lower_bounds:
-        print("ears,taco={}".format(lower_bounds[pack(ears,taco)]))
-    else:
-        print("ears,taco={}".format("undefined"))
-
     for k in list(lower_bounds.keys()):
         s = unpack(k)
         x = pack_set(s ^ {mariposa})
@@ -186,11 +182,6 @@ if __name__ == "__main__":
             and lower_bounds[x] != lower_bounds[k]:
             print("Warning: lower bound {} != {}".format(tostring(x), tostring(k)))
         lower_bounds[x] = lower_bounds[k]
-
-    if pack(ears,taco) in lower_bounds:
-        print("ears,taco={}".format(lower_bounds[pack(ears,taco)]))
-    else:
-        print("ears,taco={}".format("undefined"))
 
     # Create all upper bounds inherited by superset relationships
     for k in list(upper_bounds.keys()):
@@ -201,13 +192,6 @@ if __name__ == "__main__":
             if x not in upper_bounds or upper_bounds[x] > upper_bounds[k]:
                 upper_bounds[x] = upper_bounds[k]
 
-    if pack(ears,taco) in lower_bounds:
-        print("ears,taco={}".format(lower_bounds[pack(ears,taco)]))
-    else:
-        print("ears,taco={}".format("undefined"))
-
-
-
     # Create all lower bounds inherited by subset relationship
     for k in list(lower_bounds.keys()):
         these = set(unpack(k))
@@ -216,22 +200,6 @@ if __name__ == "__main__":
             if x not in lower_bounds or lower_bounds[x] < lower_bounds[k]:
                 lower_bounds[x] = lower_bounds[k]
 
-    if pack(ears,taco) in lower_bounds:
-        print("ears,taco={}".format(lower_bounds[pack(ears,taco)]))
-    else:
-        print("ears,taco={}".format("undefined"))
-
-
-
-    # Print out everything we know
-    print_everything(upper_bounds, lower_bounds)
-
-    print("swords={}".format(upper_bounds[pack(swords)]))
-
     fp = open("bounds.tex", "w")
     make_table(fp, upper_bounds, lower_bounds)
     fp.close()
-
-    #fp = open("lower.tex", "w")
-    #make_table(fp, lower_bounds, upper_bounds)
-    #fp.close()
